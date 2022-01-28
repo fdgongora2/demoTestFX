cd71d7f0a1153e79a77d893774f6395cfb68a449
fdgongora <fdgongora@iesmurgi.org>
2022-01-28 10:14:09 +0100

    Nueva ventana emergente - Se abre

---
+++ src/main/java/com/example/demotestfx3/DataView.java
@@ -0,0 +1,11 @@
+package com.example.demotestfx3;
+
+import javafx.stage.Stage;
+
+public class DataView {
+    private Stage stage ;
+
+    public void initialize() {
+       // this.stage.setTitle("Ventana de datos");
+    }
+}
--- src/main/java/com/example/demotestfx3/HelloController.java
+++ src/main/java/com/example/demotestfx3/HelloController.java
@@ -1,14 +1,42 @@
 package com.example.demotestfx3;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private Button btnDatos;
+    @FXML
+    private Button boton;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    public void onButtonDatosClick(ActionEvent actionEvent) {
+        Parent root;
+        try {
+            root = FXMLLoader.load(HelloApplication.class.getResource("data-view.fxml"));
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root, 450, 450));
+            stage.show();
+            // Hide this current window (if this is what you want)
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
